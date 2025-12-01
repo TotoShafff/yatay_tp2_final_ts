be5f499663fe1a14c16523d953650e9b28dfa360
Tobias <tobiaspsp@hotmail.com>
2025-12-01 19:29:33 -0300

    fix: changed names in index

--- config/index.js
+++ config/index.js
@@ -4,11 +4,11 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   
-  // Database
+  // Db
   DB_PROVIDER: process.env.DB_PROVIDER || 'mongo',
   MONGODB_URI: process.env.MONGODB_URI,
   
-  // Security
+  // Seguridad y claves
   API_KEY: process.env.API_KEY,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRATION: process.env.JWT_EXPIRATION || '24h'
